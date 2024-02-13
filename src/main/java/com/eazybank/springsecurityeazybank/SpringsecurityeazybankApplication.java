1e751da3ee494e46addd19ee30ddf7a5875cf935
hiyadav <himanshu.yadav@soprabanking.com>
2024-02-14 02:28:07 +0530

    feat: add config to create SecurityFilterChain that allow acces to few resource

--- src/main/java/com/eazybank/springsecurityeazybank/SpringsecurityeazybankApplication.java
+++ src/main/java/com/eazybank/springsecurityeazybank/SpringsecurityeazybankApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("com.eazybank.springsecurityeazybank.controller")//Optional - used only when we want any particular to be scanned
+// @ComponentScan("com.eazybank.springsecurityeazybank.controller")//Optional - used only when we want any particular to be scanned
 public class SpringsecurityeazybankApplication {
 
 	public static void main(String[] args) {
