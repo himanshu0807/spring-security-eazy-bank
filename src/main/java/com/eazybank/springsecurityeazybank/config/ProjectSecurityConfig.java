c83b5ed51d290db8da5549ccf6e13ac60d546ac7
hiyadav <himanshu.yadav@soprabanking.com>
2024-02-14 03:08:45 +0530

    feat: create multiple user using inMemorymanager to UserDetails

--- src/main/java/com/eazybank/springsecurityeazybank/config/ProjectSecurityConfig.java
+++ src/main/java/com/eazybank/springsecurityeazybank/config/ProjectSecurityConfig.java
@@ -3,6 +3,9 @@ package com.eazybank.springsecurityeazybank.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -12,16 +15,26 @@ public class ProjectSecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        /*http.authorizeHttpRequests(
+        http.authorizeHttpRequests(
                 (requests) -> requests.requestMatchers("/myAccount", "/myBalance", "/myLoans", "myCards").authenticated()
                                       .requestMatchers("/notices", "/contact").permitAll());
-        http.formLogin(withDefaults());
-        http.httpBasic(withDefaults());*/
-
-        http.authorizeHttpRequests(request -> request.anyRequest().denyAll());
         http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
         return http.build();
     }
 
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager(){
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                                .username("admin")
+                                .password("admin123")
+                                .authorities("admin")
+                                .build();
+        UserDetails user = User.withDefaultPasswordEncoder()
+                                .username("user")
+                                .password("12345")
+                                .authorities("read")
+                                .build();
+        return new InMemoryUserDetailsManager(admin,user);
+    }
 }
