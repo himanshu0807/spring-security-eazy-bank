d08d9a03f9d661626eb0ce681d9786920e688560
hiyadav <himanshu.yadav@soprabanking.com>
2024-02-18 02:37:51 +0530

    feat: models added in reference to hibernate JPA

---
+++ src/main/java/com/eazybank/springsecurityeazybank/model/Loans.java
@@ -0,0 +1,102 @@
+package com.eazybank.springsecurityeazybank.model;
+
+import java.sql.Date;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name="loans")
+public class Loans {
+	
+	@Id
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
+	@Column(name = "loan_number")
+	private int loanNumber;
+	
+	@Column(name = "customer_id")
+	private int customerId;
+	
+	@Column(name="start_dt")
+	private Date startDate;
+	
+	@Column(name = "loan_type")
+	private String loanType;
+	
+	@Column(name = "total_loan")
+	private int totalLoan;
+	
+	@Column(name = "amount_paid")
+	private int amountPaid;
+	
+	@Column(name = "outstanding_amount")
+	private int outstandingAmount;
+	
+	@Column(name = "create_dt")
+	private Date createDate;
+
+	public int getLoanNumber() {
+		return loanNumber;
+	}
+
+	public void setLoanNumber(int loanNumber) {
+		this.loanNumber = loanNumber;
+	}
+
+	public int getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
+	}
+
+	public Date getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+
+	public String getLoanType() {
+		return loanType;
+	}
+
+	public void setLoanType(String loanType) {
+		this.loanType = loanType;
+	}
+
+	public int getTotalLoan() {
+		return totalLoan;
+	}
+
+	public void setTotalLoan(int totalLoan) {
+		this.totalLoan = totalLoan;
+	}
+
+	public int getAmountPaid() {
+		return amountPaid;
+	}
+
+	public void setAmountPaid(int amountPaid) {
+		this.amountPaid = amountPaid;
+	}
+
+	public int getOutstandingAmount() {
+		return outstandingAmount;
+	}
+
+	public void setOutstandingAmount(int outstandingAmount) {
+		this.outstandingAmount = outstandingAmount;
+	}
+
+	public Date getCreateDate() {
+		return createDate;
+	}
+
+	public void setCreateDate(Date createDate) {
+		this.createDate = createDate;
+	}
+	
+}
